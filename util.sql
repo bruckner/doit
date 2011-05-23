117094cb6dac4b7e33947c421a6bc354611545cf
Daniel Bruckner <dmeirb@gmail.com>
2011-05-23 18:18:05 -0400

    added function for getting random source sets

--- util.sql
+++ util.sql
@@ -1,3 +1,20 @@
+
+-- Generate a list of random source_ids
+-- First arg in # sources, second it max source size (in entities)
+create or replace function random_source_list (integer, integer) returns setof integer
+as '
+select source_id
+  from (
+    select source_id, random() x
+      from doit_sources
+     where n_entities <= $2
+  order by x desc
+     limit $1
+  ) t;
+' language sql;
+
+
+-- Exists functions
 create or replace function table_exists (tname text, sname text default 'public')
        returns boolean
 as '
@@ -22,4 +39,13 @@ as '
    	  (select viewname from pg_views
    	    where viewname = $1
 	      and schemaname = $2);
+' language sql;
+
+create or replace function index_exists (iname text)
+       returns boolean
+as '
+   select exists
+   	  (select indexname
+	     from pg_indexes
+	    where indexname = $1);
 ' language sql;
