08ee517dcb477e27b631f28fb6785698f11a5ead
Daniel Bruckner <dmeirb@gmail.com>
2011-06-30 16:18:39 -0400

    training UDFs improved/simplified for use with deterministic source list

--- nameres/training.sql
+++ nameres/training.sql
@@ -9,6 +9,7 @@ DROP VIEW IF EXISTS training_data CASCADE;
 -- Sources to use for training kept here
 CREATE TABLE training_sources (source_id integer);
 
+
 -- Useful views
 CREATE VIEW training_fields AS
 SELECT source_id, name, tag_code
@@ -23,39 +24,31 @@ CREATE VIEW training_data AS
          IN (SELECT source_id FROM training_sources)
 	AND value IS NOT NULL;
 
+
 -- UDF to load training data into DoIt
 CREATE OR REPLACE FUNCTION training_stage (integer, integer) RETURNS void AS
 $$
 BEGIN
 
--- Get some random training sources
-INSERT INTO training_sources
-     SELECT * FROM random_source_list($1,$2);
+  -- Get some random training sources
+  INSERT INTO training_sources
+       SELECT * FROM random_source_list($1,$2);
 
--- Load training data into staging area
-INSERT INTO in_sources (source_id)
-     SELECT * FROM training_sources;
+  -- Stage training data
+  PERFORM staging_load_source(source_id) FROM training_sources;
 
-INSERT INTO in_fields (source_id, name)
-     SELECT source_id, name
-       FROM training_fields;
+  -- Load training tags as global atts
+  INSERT INTO global_attributes (source_id, name)
+       SELECT MIN(source_id), tag_code
+         FROM training_fields
+     GROUP BY tag_code;
 
-INSERT INTO in_data (source_id, entity_id, name, value)
-     SELECT source_id, local_entity_id, name, value
-       FROM training_data;
-
--- Load training tags as global atts
-INSERT INTO global_attributes (source_id, name)
-     SELECT MIN(source_id), tag_code
-       FROM training_fields
-   GROUP BY tag_code;
-
--- Create training attribute clusters
-INSERT INTO attribute_clusters (global_id, global_name, local_source_id,
-                                local_name, uncertainty, authority)
-     SELECT g.id, g.name, t.source_id, t.name, 0.0, 1.0
-       FROM global_attributes g, training_fields t
-      WHERE g.name = t.tag_code;
+  -- Create training attribute clusters
+  INSERT INTO attribute_clusters (global_id, global_name, local_source_id,
+                                  local_name, uncertainty, authority)
+       SELECT g.id, g.name, t.source_id, t.name, 0.0, 1.0
+         FROM global_attributes g, training_fields t
+        WHERE g.name = t.tag_code;
 
 END
 $$ LANGUAGE plpgsql;
