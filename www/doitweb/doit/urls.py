93c124bdd5ca1670c884dc13fa47b7f1080699f8
Daniel Bruckner <dmeirb@gmail.com>
2012-02-07 15:05:20 -0500

    added NEDBS demo features: source browser, table viewer, etc.

--- www/doitweb/doit/urls.py
+++ www/doitweb/doit/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('doit.views',
+    (r'^templates/viewTable', 'viewTable_template'),
     (r'^(?P<dbname>\w+)/$', 'source_index'),
 
     (r'^(?P<dbname>\w+)/lowscorers/$', 'lowscoremapper'),
     (r'^(?P<dbname>\w+)/sources/(?P<sid>\d+)/map$', 'mapper'),
+    (r'^(?P<dbname>\w+)/sources/(?P<sid>\d+)/data', 'source_data'),
+    (r'^(?P<dbname>\w+)/sources/(?P<sid>\d+)/table', 'source_table'),
+    (r'^(?P<dbname>\w+)/sources/(?P<sid>\d+)/entities', 'source_entities'),
     (r'^(?P<dbname>\w+)/sources/(?P<sid>\d+)/process/(?P<method_index>[0-4])/', 'source_processor'),
     (r'^(?P<dbname>\w+)/fields/(?P<comp_op>(like|named))/(?P<field_name>\w+)/map$', 'mapper_by_field_name'),
     (r'^(?P<dbname>\w+)/save$', 'mapper_results'),
